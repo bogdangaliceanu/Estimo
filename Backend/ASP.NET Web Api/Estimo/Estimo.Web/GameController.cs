abb52c95e73c7aae1ea1c6da7fc1eb85cabd0ae0
Bogdan <bogdan.galiceanu@yahoo.com>
2017-09-05 22:01:48 +0300

    web wireup

---
+++ Backend/ASP.NET Web Api/Estimo/Estimo.Web/GameController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Estimo.Web
+{
+    public class GameController : ApiController
+    {
+        [HttpPost, Route("game")]
+        public HttpResponseMessage NewGame()
+        {
+            var game = new Game();
+
+            var response = new HttpResponseMessage(HttpStatusCode.Created);
+            response.Headers.Location = new Uri(game.Id.ToString(), UriKind.Relative);
+            return response;
+        }
+    }
+}
